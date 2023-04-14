9267f6b0a4fe7dea657a99c2073b43c863c78f15
preetsagar <ps792001@gmail.com>
2023-04-14 23:27:22 +0530

    updated server.js

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ mongoose
     console.log("Connected with the DATABASE");
   });
 
-app.listen(process.env.PORT || 3000, "127.0.0.1", () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server Running on PORT 3000");
 });
